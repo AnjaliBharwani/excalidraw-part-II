176d71ef65661d1cb8c396ec8ad0840899d162bd
AnjaliBharwani <acksacks2405@gmail.com>
2023-10-22 02:38:16 +0530

    excalidraw

--- freehands.js
+++ freehands.js
@@ -1,30 +1,26 @@
 const pencil = document.getElementById("pencil");
-let isPencilActive = false ;
+let isPencilActive = false ;// intially the pencil is inactive.
 
 const colorPicker = document.getElementById("color-picker");
 
 
 colorPicker.addEventListener("change", () => {
-
     drawingColor = colorPicker.value ;
-    c.strokeStyle = drawingColor;
+    console.log(drawingColor);
 });
 
-pencil.addEventListener("click", onPencilClick) ;
-
 function onPencilClick() {
-    pencil.classList.toggle("active");
-    
-
-    isPencilActive = !isPencilActive; 
-    
+    pencil.classList.toggle("active")
+    isPencilActive = !isPencilActive ; // enabling the drawing 
+    // isPencilActive = !false = true
     if(isPencilActive) {
-
         canvas.style.cursor = "crosshair";
         canvas.addEventListener("mousedown", onMouseDown); 
     }
     else {
         canvas.style.cursor = "auto";
-        canvas.removeEventListener("mousedown", onMouseDown);
+        canvas.removeEventListener("mousedown", onMouseDown)
     }
 }
+
+pencil.addEventListener("click", onPencilClick) ;
--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-const canvas=document.getElementById("canvas");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+const canvas = document.getElementById("canvas");
 
-const c=canvas.getContext("2d");
+canvas.width = window.innerWidth ;
+canvas.height = window.innerHeight ;
 
-let drawingColor = "black";
 
+const c = canvas.getContext("2d");
+
+function drawLine(p1, p2, color = "blue", thickness = 2) {
+    c.beginPath();
+    c.strokeStyle = color ;
+    c.lineWidth = thickness; 
+    c.moveTo(p1.x, p1.y);
+    c.lineTo(p2.x, p2.y);
+    c.stroke();
+    c.closePath();
+}   
+let drawingColor = "blue";
 let previousPosition = null ;
 
 function onMouseDown(e) {
-    console.log("mouse down");
     previousPosition = [ e.clientX , e.clientY];
+    c.strokeStyle = drawingColor;
     c.lineWidth = 2; 
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mouseup", onMouseUp); 
 }
 
 function onMouseMove(e){ 
+    // for the first time inside this  
     let currentPosition = [ e.clientX , e.clientY ];
-
-    console.log(currentPosition);
+    // draw line from previous position to current position ;
     c.beginPath();
     c.moveTo(...previousPosition);
     c.lineTo(...currentPosition);
